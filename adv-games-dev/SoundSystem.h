412c83607059ab4dae4ea9e00de0133f43fbdaa7
x4iiiis <40168766@napier.ac.uk>
2017-12-05 15:37:21 +0000

    Messing with sound - not sure if this is gonna work cause I don't have earphones to test it in the lab but I'll look at it later. Now looking towards multi-threading collision detection

---
+++ adv-games-dev/SoundSystem.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <iostream>
+#include <Windows.h>
+#include <mmsystem.h>
+
+using namespace std;
+
+class SoundSystem
+{
+public:
+	void PlayMenuMusic()
+	{
+		PlaySound(TEXT("test.wav"), NULL, SND_FILENAME);
+
+		//system("pause");
+	}
+
+	void PlayGameMusic()
+	{
+		PlaySound(TEXT("filename.wav"), NULL, SND_FILENAME);
+	}
+
+	void PlayShootingSound()
+	{
+		PlaySound(TEXT("filename.wav"), NULL, SND_FILENAME);
+	}
+
+	void PauseSoundSystem()
+	{
+		system("pause");
+	}
+};
