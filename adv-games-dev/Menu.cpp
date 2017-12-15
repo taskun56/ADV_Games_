dda8e3d7e1413c41bcd77a0ad52e3268515a3936
taskun56 <40182055@live.napier.ac.uk>
2017-12-15 06:19:16 +0000

    Update Menu.cpp

--- adv-games-dev/Menu.cpp
+++ adv-games-dev/Menu.cpp
@@ -44,7 +44,7 @@ public:
 
 		Uint32 time;
 		int x, y;
-		const char *labels[NUMMENU] = { "HSART: SPACE WARRIOR","Start","Options","Quit" };
+		const char *labels[NUMMENU] = { "HSAЯT: SPACE WARRIOR","Start","Options","Quit" };
 		bool selected[NUMMENU] = { 0,0,0,0 };
 		SDL_Texture* background;
 		SDL_Surface* background_surface;
